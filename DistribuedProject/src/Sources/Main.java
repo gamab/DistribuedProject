8f756f22f22887021260be9674c1afae91131fd1
Mabille <gabi.mabs@gmail.com>
2013-11-10 11:48:55 -0300

    Got rid of the default package

---
+++ DistribuedProject/src/Sources/Main.java
@@ -0,0 +1,12 @@
+package Sources;
+import Test.TestCRWaitingListCell;
+import Test.TestParticipant;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		TestParticipant.runTest();
+		TestCRWaitingListCell.runTest();
+	}
+}
