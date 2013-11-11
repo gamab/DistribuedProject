bf4d52ba63dd7632f5e35d673e32e3b29df7c031
Mabille <gabi.mabs@gmail.com>
2013-11-11 12:31:47 -0300

    The constructor now receives everything

--- DistribuedProject/src/Sources/ServiceThread.java
+++ DistribuedProject/src/Sources/ServiceThread.java
@@ -9,13 +9,15 @@ public class ServiceThread extends Thread {
 
 	private DatagramSocket serviceSocket;
 	private CRWaitingList crWaitingList;
+	private String[] resources;
 	private ParticipantList participants;
 	private int pid;
 	private LogicalClock clock;
 	
-	public ServiceThread(CRWaitingList crWaitingList,
+	public ServiceThread(DatagramSocket serviceSocket, CRWaitingList crWaitingList,
 			ParticipantList participants, int pid, LogicalClock clock) {
 		super();
+		this.serviceSocket = serviceSocket;
 		this.crWaitingList = crWaitingList;
 		this.participants = participants;
 		this.pid = pid;
@@ -26,8 +28,13 @@ public class ServiceThread extends Thread {
 		
 	}
 	
+	// In function of the request we received we are returning an answer thanks to the protocol we made
 	private String treatRequest(String request) {
-		return null;
+		String answer = null;
+		if (request.equals("GET_RESSOURCE")) {
+			
+		}
+		return answer;
 	}
 	
 	private void sendAnswer(String answer, InetAddress ip, Integer port) {
