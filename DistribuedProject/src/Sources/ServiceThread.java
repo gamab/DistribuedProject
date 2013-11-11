c4fcb4b5382eb08c997bb490dde5b23d8104b88b
Mabille <gabi.mabs@gmail.com>
2013-11-11 12:25:29 -0300

    Created API of the serviceThread

--- DistribuedProject/src/Sources/ServiceThread.java
+++ DistribuedProject/src/Sources/ServiceThread.java
@@ -1,6 +1,7 @@
 package Sources;
 
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 import CriticalResources.CRWaitingList;
 
@@ -9,4 +10,28 @@ public class ServiceThread extends Thread {
 	private DatagramSocket serviceSocket;
 	private CRWaitingList crWaitingList;
 	private ParticipantList participants;
+	private int pid;
+	private LogicalClock clock;
+	
+	public ServiceThread(CRWaitingList crWaitingList,
+			ParticipantList participants, int pid, LogicalClock clock) {
+		super();
+		this.crWaitingList = crWaitingList;
+		this.participants = participants;
+		this.pid = pid;
+		this.clock = clock;
+	}
+	
+	public void run() {
+		
+	}
+	
+	private String treatRequest(String request) {
+		return null;
+	}
+	
+	private void sendAnswer(String answer, InetAddress ip, Integer port) {
+		
+	}
+	
 }
