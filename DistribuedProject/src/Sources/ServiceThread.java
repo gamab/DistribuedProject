e009a8f245f0f6e018a6f1fbb806cb9e8c54c119
Mabille <gabi.mabs@gmail.com>
2013-11-11 23:35:32 -0300

    Add forgotten to increment the clock after sending

--- DistribuedProject/src/Sources/ServiceThread.java
+++ DistribuedProject/src/Sources/ServiceThread.java
@@ -38,6 +38,7 @@ public class ServiceThread extends Thread {
 			request = DatagramCommunication.retrieveMessage(serviceSocket);
 			answer = treatRequest(request.getMessage());
 			DatagramCommunication.sendMessage(answer,serviceSocket,request.getIpOrigin(),request.getPortOrigin());
+			this.clock.incClock();
 		}
 	}
 	
@@ -45,81 +46,84 @@ public class ServiceThread extends Thread {
 	private String treatRequest(String request) {
 		String answer = null;
 		
-		//we split the request with "{{" to have access to the different pieces of data contained by the message
-		String[] datas = request.split("{{");
+		System.out.println("In ServiceThread : in treatRequest : Received " + request);
+		//we split the request with "<<" to have access to the different pieces of data contained by the message
+		String[] datas = request.split("<<");
+		for (int i = 0; i<datas.length; i++) {
+			System.out.println("datas[" + i + "] = " + datas[i]	);
+		}
 		//get the clock
 		String clockStrg = datas[datas.length-1];
-		clockStrg = clockStrg.split("}}")[0];
 		//set the clock logically (max(x,y+1))
 		try {
 			int messageClock = Integer.valueOf(clockStrg);
 			this.clock.setClockLogically(messageClock);
 		} catch (NumberFormatException e) {
-			answer = "COULD_NOT_GET_CLOCK{{" + this.clock.getClock() + "}}";
+			answer = "COULD_NOT_GET_CLOCK<<" + this.clock.getClock() + "<<";
 		}
 		
 		if (request.startsWith("GET_RESOURCE")) {
-			String resourceName = datas[1].split("}}")[0];
+			String resourceName = datas[1];
 			boolean resourceFound = false;
 			for (int i=0; i < resources.length && !resourceFound; i++) {
 				if (resourceName.equals(resources[i])) {
-					answer = "OK_IT_IS_YOURS{{"+ this.clock.getClock() + "}}";
+					answer = "OK_IT_IS_YOURS<<"+ this.clock.getClock() + "<<";
 					resourceFound = true;
 				}
 			}
 			if (!resourceFound) {
-				answer = "I_DO_NOT_HAVE_IT{{"+ this.clock.getClock() + "}}";
+				answer = "I_DO_NOT_HAVE_IT<<"+ this.clock.getClock() + "<<";
 			}
 		}
 		else if (request.startsWith("FREE_RESOURCE")) {
-			String resourceName = datas[1].split("}}")[0];
+			String resourceName = datas[1];
 			boolean resourceFound = false;
 			for (int i=0; i < resources.length && !resourceFound; i++) {
 				if (resourceName.equals(resources[i])) {
-					answer = "OK_IT_IS_NOT_YOURS_ANYMORE{{"+ this.clock.getClock() + "}}";
+					answer = "OK_IT_IS_NOT_YOURS_ANYMORE<<"+ this.clock.getClock() + "<<";
 					resourceFound = true;
 				}
 			}
 			if (!resourceFound) {
-				answer = "I_DO_NOT_HAVE_IT{{"+ this.clock.getClock() + "}}";
+				answer = "I_DO_NOT_HAVE_IT<<"+ this.clock.getClock() + "<<";
 			}
 		}
 		else if (request.startsWith("JOIN")) {
-			String partStrg = datas[1].split("}}")[0];
+			String partStrg = datas[1];
 			Participant p = new Participant();
 			boolean partOK = p.fromString(partStrg);
 			if (partOK) {
 				participants.add(p);
-				answer = "OK_I_ADD_YOU{{" + this.clock.getClock() + "}}";
+				answer = "OK_I_ADD_YOU<<" + this.clock.getClock() + "<<";
 			} else {
-				answer = "BAD_FORMAT{{" + this.clock.getClock() + "}}";
+				answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
 			}
 		}
 		else if (request.startsWith("GET_PARTICIPANTS")) {
-			answer = "PARTICIPANTS{{" + participants.toString() + "}}";
+			answer = "PARTICIPANTS<<" + participants.toString() + "<<";
 		}
 		else if (request.startsWith("GET_CRITICAL_REGION")) {
-			String cellStrg = datas[1].split("}}")[0];
+			String cellStrg = datas[1];
 			CRWaitingListCell cell = new CRWaitingListCell();
 			boolean cellOK = cell.fromString(cellStrg);
 			if (cellOK) {
 				// we add the participant to the list
 				this.crWaitingList.add(cell);
-				answer = "OK{{" + this.clock.getClock() + "}}";
+				answer = "OK<<" + this.clock.getClock() + "<<";
 			} else {
-				answer = "BAD_FORMAT{{" + this.clock.getClock() + "}}";
+				answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
 			}
 		}
 		else if (request.startsWith("FREE_CRITICAL_REGION")) {
-			String cellStrg = datas[1].split("}}")[0];
+			String cellStrg = datas[1].split("<<")[0];
 			CRWaitingListCell cell = new CRWaitingListCell();
 			boolean cellOK = cell.fromString(cellStrg);
 			if (cellOK) {
 				// we remove the participant from the list
 				this.crWaitingList.remove(cell);
-				answer = "OK{{" + this.clock.getClock() + "}}";
+				answer = "OK<<" + this.clock.getClock() + "<<";
 			} else {
-				answer = "BAD_FORMAT{{" + this.clock.getClock() + "}}";
+				answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
 			}
 		}
 		return answer;
