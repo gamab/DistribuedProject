384f12d40f50736732cceb552301b3f60f7106ae
Mabille <gabi.mabs@gmail.com>
2013-11-12 01:22:41 -0300

    Finalized the tests

--- DistribuedProject/src/Test/TestServiceThread.java
+++ DistribuedProject/src/Test/TestServiceThread.java
@@ -37,6 +37,18 @@ public class TestServiceThread {
 		System.out.println("############################");
 		testCRWaitingList_10();
 		System.out.println("############################");
+		testCRWaitingList_11();
+		System.out.println("############################");
+		testCRWaitingList_12();
+		System.out.println("############################");
+		testCRWaitingList_13();
+		System.out.println("############################");
+		testCRWaitingList_14();
+		System.out.println("############################");
+		testCRWaitingList_15();
+		System.out.println("############################");
+		testCRWaitingList_16();
+		System.out.println("############################");
 		testCRWaitingList_20();
 		System.out.println("############################");
 		testCRWaitingList_21();
@@ -55,29 +67,29 @@ public class TestServiceThread {
 		crWaitingList2.add(cell3);
 		crWaitingList2.add(cell4);
 		CRWaitingList[] crWaitingLists = {crWaitingList1,crWaitingList2};
-		
+
 		String[] resources = {"A1","B2"};
-	
+
 		ArrayList<String> resource1 = new ArrayList<String>();
 		resource1.add("A1");
 		resource1.add("B2");
 		ArrayList<String> resource2 = new ArrayList<String>();
 		resource2.add("A2");
 		resource2.add("B3");
-		
+
 		ParticipantList participants = new ParticipantList();
 		Participant p1 = new Participant(1900, s.getLocalPort(),resource1);
 		Participant p2 = new Participant(1910, 4923,resource2);
 		participants.add(p1);
 		participants.add(p2);
-		
+
 		int pid = 1900;
 		LogicalClock clock = new LogicalClock();
 
 		ServiceThread service = new ServiceThread(s, crWaitingLists,resources,participants,pid,clock);
 		return service;	
 	}
-	
+
 	public static void testCRWaitingList_1() {
 		System.out.println("Test n°1");
 		System.out.println("Description : Test");
@@ -103,7 +115,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -112,7 +124,7 @@ public class TestServiceThread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void testCRWaitingList_2() {
 		System.out.println("Test n°2");
 		System.out.println("Description : Test");
@@ -138,7 +150,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -147,7 +159,7 @@ public class TestServiceThread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void testCRWaitingList_3() {
 		System.out.println("Test n°3");
 		System.out.println("Description : Test");
@@ -177,7 +189,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -186,7 +198,7 @@ public class TestServiceThread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void testCRWaitingList_4() {
 		System.out.println("Test n°4");
 		System.out.println("Description : Test");
@@ -216,7 +228,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -226,7 +238,7 @@ public class TestServiceThread {
 		}
 	}
 
-	
+
 	public static void testCRWaitingList_5() {
 		System.out.println("Test n°5");
 		System.out.println("Description : Test");
@@ -253,7 +265,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -262,7 +274,7 @@ public class TestServiceThread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void testCRWaitingList_6() {
 		System.out.println("Test n°6");
 		System.out.println("Description : Test");
@@ -289,7 +301,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -298,7 +310,7 @@ public class TestServiceThread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void testCRWaitingList_7() {
 		System.out.println("Test n°7");
 		System.out.println("Description : Test");
@@ -331,7 +343,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -366,7 +378,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -402,7 +414,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -411,7 +423,7 @@ public class TestServiceThread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void testCRWaitingList_10() {
 		System.out.println("Test n°10");
 		System.out.println("Description : Test");
@@ -447,7 +459,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -456,7 +468,7 @@ public class TestServiceThread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void testCRWaitingList_11() {
 		System.out.println("Test n°11");
 		System.out.println("Description : Test");
@@ -469,18 +481,12 @@ public class TestServiceThread {
 			s1 = new DatagramSocket(0);
 			ServiceThread serviceT = buildServiceThread(serviceSocket);
 			serviceT.start();
-			Participant participant ;
-			participant = new Participant(1902,24932);
-			participant.addToResourcesList("A8");
-			participant.addToResourcesList("B9");
-			participant.addToResourcesList("C10");
-			participant.addToResourcesList("D11");
-			String message1 = "JOIN<<"+participant.toString()+"<<20<<";
-			DatagramCommunication.sendMessage(message1, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
-			DatagramCommunication.retrieveMessage(s1);
-			String message2 = "GET_PARTICIPANTS"+"<<22<<";
-			DatagramCommunication.sendMessage(message2, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
-			String attendu = "PARTICIPANTS<<1900//"+ serviceSocket.getLocalPort() +"//A1;;B2:::1910//4923//A2;;B3:::1902//24932//A8;;B9;;C10;;D11<<23<<";
+
+			CRWaitingListCell cell = new CRWaitingListCell(1902, 45);
+
+			String message = "GET_CRITICAL_REGION<<1<<"+cell.toString()+"<<45<<";
+			DatagramCommunication.sendMessage(message, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
+			String attendu = "OK<<46<<";
 			String obtenu =  DatagramCommunication.retrieveMessage(s1).getMessage();
 
 			System.out.println("Attendu : " + attendu);
@@ -492,7 +498,46 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
+			serviceT.stop();
+			serviceSocket.close();
+			s1.close();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void testCRWaitingList_12() {
+		System.out.println("Test n°12");
+		System.out.println("Description : Test");
+		System.out.println("Description : GET_CRITICAL_REGION imposible because it is the third of two CR");
+
+		DatagramSocket serviceSocket;
+		DatagramSocket s1;
+		try {
+			serviceSocket = new DatagramSocket(0);
+			s1 = new DatagramSocket(0);
+			ServiceThread serviceT = buildServiceThread(serviceSocket);
+			serviceT.start();
+
+			CRWaitingListCell cell = new CRWaitingListCell(1902, 45);
+
+			String message = "GET_CRITICAL_REGION<<2<<"+cell.toString()+"<<45<<";
+			DatagramCommunication.sendMessage(message, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
+			String attendu = "BAD_FORMAT<<46<<";
+			String obtenu =  DatagramCommunication.retrieveMessage(s1).getMessage();
+
+			System.out.println("Attendu : " + attendu);
+			System.out.println("Obtenu  : " + obtenu);
+
+			if (attendu.equals(obtenu)) {
+				System.out.println("Test OK");
+			}
+			else {
+				System.out.println("Test failed");
+			}
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -502,7 +547,166 @@ public class TestServiceThread {
 		}
 	}
 	
+	public static void testCRWaitingList_13() {
+		System.out.println("Test n°13");
+		System.out.println("Description : Test");
+		System.out.println("Description : GET_CRITICAL_REGION imposible because we are not giving a proper WaitingListCell");
+
+		DatagramSocket serviceSocket;
+		DatagramSocket s1;
+		try {
+			serviceSocket = new DatagramSocket(0);
+			s1 = new DatagramSocket(0);
+			ServiceThread serviceT = buildServiceThread(serviceSocket);
+			serviceT.start();
+
+			String message = "GET_CRITICAL_REGION<<1<<CELL_POORLY_MADE<<45<<";
+			DatagramCommunication.sendMessage(message, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
+			String attendu = "BAD_FORMAT<<46<<";
+			String obtenu =  DatagramCommunication.retrieveMessage(s1).getMessage();
+
+			System.out.println("Attendu : " + attendu);
+			System.out.println("Obtenu  : " + obtenu);
+
+			if (attendu.equals(obtenu)) {
+				System.out.println("Test OK");
+			}
+			else {
+				System.out.println("Test failed");
+			}
+
+			serviceT.stop();
+			serviceSocket.close();
+			s1.close();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void testCRWaitingList_14() {
+		System.out.println("Test n°14");
+		System.out.println("Description : Test");
+		System.out.println("Description : FREE_CRITICAL_REGION posible (we add a cell and then delete it)");
+
+		DatagramSocket serviceSocket;
+		DatagramSocket s1;
+		try {
+			serviceSocket = new DatagramSocket(0);
+			s1 = new DatagramSocket(0);
+			ServiceThread serviceT = buildServiceThread(serviceSocket);
+			serviceT.start();
+
+			CRWaitingListCell cell = new CRWaitingListCell(1902, 45);
+
+			String message = "GET_CRITICAL_REGION<<1<<"+cell.toString()+"<<45<<";
+			DatagramCommunication.sendMessage(message, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
+			DatagramCommunication.retrieveMessage(s1);
+			String message2 = "FREE_CRITICAL_REGION<<1<<"+cell.toString()+"<<47<<";
+			DatagramCommunication.sendMessage(message2, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
+			
+			String attendu = "OK<<48<<";
+			String obtenu =  DatagramCommunication.retrieveMessage(s1).getMessage();
+
+			System.out.println("Attendu : " + attendu);
+			System.out.println("Obtenu  : " + obtenu);
+
+			if (attendu.equals(obtenu)) {
+				System.out.println("Test OK");
+			}
+			else {
+				System.out.println("Test failed");
+			}
+
+			serviceT.stop();
+			serviceSocket.close();
+			s1.close();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void testCRWaitingList_15() {
+		System.out.println("Test n°15");
+		System.out.println("Description : Test");
+		System.out.println("Description : FREE_CRITICAL_REGION imposible because it is the third of two CR");
+
+		DatagramSocket serviceSocket;
+		DatagramSocket s1;
+		try {
+			serviceSocket = new DatagramSocket(0);
+			s1 = new DatagramSocket(0);
+			ServiceThread serviceT = buildServiceThread(serviceSocket);
+			serviceT.start();
+
+			CRWaitingListCell cell = new CRWaitingListCell(1902, 45);
+
+			String message = "GET_CRITICAL_REGION<<1<<"+cell.toString()+"<<45<<";
+			DatagramCommunication.sendMessage(message, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
+			DatagramCommunication.retrieveMessage(s1);
+			String message2 = "FREE_CRITICAL_REGION<<2<<"+cell.toString()+"<<47<<";
+			DatagramCommunication.sendMessage(message2, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
+			
+			String attendu = "BAD_FORMAT<<48<<";
+			String obtenu =  DatagramCommunication.retrieveMessage(s1).getMessage();
+
+			System.out.println("Attendu : " + attendu);
+			System.out.println("Obtenu  : " + obtenu);
+
+			if (attendu.equals(obtenu)) {
+				System.out.println("Test OK");
+			}
+			else {
+				System.out.println("Test failed");
+			}
+
+			serviceT.stop();
+			serviceSocket.close();
+			s1.close();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
+	public static void testCRWaitingList_16() {
+		System.out.println("Test n°16");
+		System.out.println("Description : Test");
+		System.out.println("Description : FREE_CRITICAL_REGION imposible because we are not giving a proper WaitingListCell");
+
+		DatagramSocket serviceSocket;
+		DatagramSocket s1;
+		try {
+			serviceSocket = new DatagramSocket(0);
+			s1 = new DatagramSocket(0);
+			ServiceThread serviceT = buildServiceThread(serviceSocket);
+			serviceT.start();
+
+			String message = "FREE_CRITICAL_REGION<<1<<CELL_POORLY_MADE<<45<<";
+			DatagramCommunication.sendMessage(message, s1, serviceSocket.getLocalAddress(), serviceSocket.getLocalPort());
+			String attendu = "BAD_FORMAT<<46<<";
+			String obtenu =  DatagramCommunication.retrieveMessage(s1).getMessage();
+
+			System.out.println("Attendu : " + attendu);
+			System.out.println("Obtenu  : " + obtenu);
+
+			if (attendu.equals(obtenu)) {
+				System.out.println("Test OK");
+			}
+			else {
+				System.out.println("Test failed");
+			}
+
+			serviceT.stop();
+			serviceSocket.close();
+			s1.close();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static void testCRWaitingList_20() {
 		System.out.println("Test n°20");
 		System.out.println("Description : Test");
@@ -529,7 +733,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
@@ -538,7 +742,7 @@ public class TestServiceThread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void testCRWaitingList_21() {
 		System.out.println("Test n°21");
 		System.out.println("Description : Test");
@@ -565,7 +769,7 @@ public class TestServiceThread {
 			else {
 				System.out.println("Test failed");
 			}
-			
+
 			serviceT.stop();
 			serviceSocket.close();
 			s1.close();
