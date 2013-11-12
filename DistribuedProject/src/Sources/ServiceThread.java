807f0b2184537b5075027442bfdb09639f567513
Mabille <gabi.mabs@gmail.com>
2013-11-12 00:55:22 -0300

    Changed the constructor and the request handling for GET and FREE RESOURCE

--- DistribuedProject/src/Sources/ServiceThread.java
+++ DistribuedProject/src/Sources/ServiceThread.java
@@ -12,26 +12,26 @@ import Participant.ParticipantList;
 public class ServiceThread extends Thread {
 
 	private DatagramSocket serviceSocket;
-	private CRWaitingList crWaitingList;
+	private CRWaitingList[] crWaitingLists;
 	private String[] resources;
 	private ParticipantList participants;
 	private int pid;
 	private LogicalClock clock;
-	
-	public ServiceThread(DatagramSocket serviceSocket, CRWaitingList crWaitingList,
+
+	public ServiceThread(DatagramSocket serviceSocket, CRWaitingList[] crWaitingLists,
 			String[] resources,ParticipantList participants, int pid,
 			LogicalClock clock) {
 		super("ServiceThread");
 		this.serviceSocket = serviceSocket;
-		this.crWaitingList = crWaitingList;
+		this.crWaitingLists = crWaitingLists;
 		this.resources = resources;
 		this.participants = participants;
 		this.pid = pid;
 		this.clock = clock;
 	}
-	
+
 	public void run() {
-		
+
 		CommunicationMessage request = new CommunicationMessage();
 		String answer = new String();
 		while(true) {
@@ -41,11 +41,12 @@ public class ServiceThread extends Thread {
 			this.clock.incClock();
 		}
 	}
-	
+
 	// In function of the request we received we are returning an answer thanks to the protocol we made
 	private String treatRequest(String request) {
 		String answer = null;
-		
+		boolean clockOK = true;
+
 		System.out.println("In ServiceThread : in treatRequest : Received " + request);
 		//we split the request with "<<" to have access to the different pieces of data contained by the message
 		String[] datas = request.split("<<");
@@ -60,74 +61,105 @@ public class ServiceThread extends Thread {
 			this.clock.setClockLogically(messageClock);
 		} catch (NumberFormatException e) {
 			answer = "COULD_NOT_GET_CLOCK<<" + this.clock.getClock() + "<<";
+			clockOK = false;
 		}
-		
-		if (request.startsWith("GET_RESOURCE")) {
-			String resourceName = datas[1];
-			boolean resourceFound = false;
-			for (int i=0; i < resources.length && !resourceFound; i++) {
-				if (resourceName.equals(resources[i])) {
-					answer = "OK_IT_IS_YOURS<<"+ this.clock.getClock() + "<<";
-					resourceFound = true;
+		if (clockOK) {
+			if (request.startsWith("GET_RESOURCE")) {
+				String resourceName = datas[1];
+				boolean resourceFound = false;
+				for (int i=0; i < resources.length && !resourceFound; i++) {
+					if (resourceName.equals(resources[i])) {
+						answer = "OK_IT_IS_YOURS<<"+ this.clock.getClock() + "<<";
+						resourceFound = true;
+					}
+				}
+				if (!resourceFound) {
+					answer = "I_DO_NOT_HAVE_IT<<"+ this.clock.getClock() + "<<";
 				}
 			}
-			if (!resourceFound) {
-				answer = "I_DO_NOT_HAVE_IT<<"+ this.clock.getClock() + "<<";
-			}
-		}
-		else if (request.startsWith("FREE_RESOURCE")) {
-			String resourceName = datas[1];
-			boolean resourceFound = false;
-			for (int i=0; i < resources.length && !resourceFound; i++) {
-				if (resourceName.equals(resources[i])) {
-					answer = "OK_IT_IS_NOT_YOURS_ANYMORE<<"+ this.clock.getClock() + "<<";
-					resourceFound = true;
+			else if (request.startsWith("FREE_RESOURCE")) {
+				String resourceName = datas[1];
+				boolean resourceFound = false;
+				for (int i=0; i < resources.length && !resourceFound; i++) {
+					if (resourceName.equals(resources[i])) {
+						answer = "OK_IT_IS_NOT_YOURS_ANYMORE<<"+ this.clock.getClock() + "<<";
+						resourceFound = true;
+					}
+				}
+				if (!resourceFound) {
+					answer = "I_DO_NOT_HAVE_IT<<"+ this.clock.getClock() + "<<";
 				}
 			}
-			if (!resourceFound) {
-				answer = "I_DO_NOT_HAVE_IT<<"+ this.clock.getClock() + "<<";
+			else if (request.startsWith("JOIN")) {
+				String partStrg = datas[1];
+				Participant p = new Participant();
+				boolean partOK = p.fromString(partStrg);
+				if (partOK) {
+					participants.add(p);
+					answer = "OK_I_ADD_YOU<<" + this.clock.getClock() + "<<";
+				} else {
+					answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
+				}
 			}
-		}
-		else if (request.startsWith("JOIN")) {
-			String partStrg = datas[1];
-			Participant p = new Participant();
-			boolean partOK = p.fromString(partStrg);
-			if (partOK) {
-				participants.add(p);
-				answer = "OK_I_ADD_YOU<<" + this.clock.getClock() + "<<";
-			} else {
-				answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
+			else if (request.startsWith("GET_PARTICIPANTS")) {
+				answer = "PARTICIPANTS<<" + participants.toString() + "<<" + this.clock.getClock() + "<<";
 			}
-		}
-		else if (request.startsWith("GET_PARTICIPANTS")) {
-			answer = "PARTICIPANTS<<" + participants.toString() + "<<";
-		}
-		else if (request.startsWith("GET_CRITICAL_REGION")) {
-			String cellStrg = datas[1];
-			CRWaitingListCell cell = new CRWaitingListCell();
-			boolean cellOK = cell.fromString(cellStrg);
-			if (cellOK) {
-				// we add the participant to the list
-				this.crWaitingList.add(cell);
-				answer = "OK<<" + this.clock.getClock() + "<<";
-			} else {
-				answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
+			else if (request.startsWith("GET_CRITICAL_REGION")) {
+				boolean nroOK;
+				boolean cellOK = true;
+				int nroCriticalRegion = -1;
+				try {
+					nroCriticalRegion = Integer.valueOf(datas[1]);
+				} catch (NumberFormatException e) {
+					nroCriticalRegion = -1;
+				}
+				//if the nro of RC is between 0 and the crWaitingLists length then it is ok
+				nroOK = nroCriticalRegion >= 0 && nroCriticalRegion < this.crWaitingLists.length;
+				if (nroOK){
+					String cellStrg = datas[2];
+					CRWaitingListCell cell = new CRWaitingListCell();
+					cellOK = cell.fromString(cellStrg);
+					if (cellOK) {
+						// we add the participant to the list
+						this.crWaitingLists[nroCriticalRegion].add(cell);
+						answer = "OK<<" + this.clock.getClock() + "<<";
+					}
+				} 
+				if (!nroOK || !cellOK) {
+					answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
+				}
 			}
-		}
-		else if (request.startsWith("FREE_CRITICAL_REGION")) {
-			String cellStrg = datas[1].split("<<")[0];
-			CRWaitingListCell cell = new CRWaitingListCell();
-			boolean cellOK = cell.fromString(cellStrg);
-			if (cellOK) {
-				// we remove the participant from the list
-				this.crWaitingList.remove(cell);
-				answer = "OK<<" + this.clock.getClock() + "<<";
-			} else {
-				answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
+			else if (request.startsWith("FREE_CRITICAL_REGION")) {
+				boolean nroOK;
+				boolean cellOK = true;
+				int nroCriticalRegion = -1;
+				try {
+					nroCriticalRegion = Integer.valueOf(datas[1]);
+				} catch (NumberFormatException e) {
+					nroCriticalRegion = -1;
+				}
+				//if the nro of RC is between 0 and the crWaitingLists length then it is ok
+				nroOK = nroCriticalRegion >= 0 && nroCriticalRegion < this.crWaitingLists.length;
+				if (nroOK){
+					String cellStrg = datas[2];
+					CRWaitingListCell cell = new CRWaitingListCell();
+					cellOK = cell.fromString(cellStrg);
+					if (cellOK) {
+						// we add the participant to the list
+						this.crWaitingLists[nroCriticalRegion].remove(cell);
+						answer = "OK<<" + this.clock.getClock() + "<<";
+					}
+				} 
+				if (!nroOK || !cellOK) {
+					answer = "BAD_FORMAT<<" + this.clock.getClock() + "<<";
+				}
+			}
+			else {
+				answer = "BAD_REQUEST";
 			}
 		}
 		return answer;
 	}
-	
-	
+
+
 }
