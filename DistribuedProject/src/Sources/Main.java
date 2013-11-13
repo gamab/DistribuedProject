b1b2dc98f687d65626f2de0627b388df6963c47f
Mabille <gabi.mabs@gmail.com>
2013-11-13 10:45:11 -0300

    Tests

--- DistribuedProject/src/Sources/Main.java
+++ DistribuedProject/src/Sources/Main.java
@@ -3,16 +3,18 @@ import Test.TestCRWaitingList;
 import Test.TestCRWaitingListCell;
 import Test.TestDatagramCommunication;
 import Test.TestParticipant;
+import Test.TestProcessus;
 import Test.TestServiceThread;
 
 
 public class Main {
 
 	public static void main(String[] args) {
-		TestParticipant.runTest();
-		TestCRWaitingListCell.runTest();
-		TestCRWaitingList.runTest();
-		TestDatagramCommunication.runTest();
-		TestServiceThread.runTest();
+//		TestParticipant.runTest();
+//		TestCRWaitingListCell.runTest();
+//		TestCRWaitingList.runTest();
+//		TestDatagramCommunication.runTest();
+//		TestServiceThread.runTest();
+		TestProcessus.runTest();
 	}
 }
