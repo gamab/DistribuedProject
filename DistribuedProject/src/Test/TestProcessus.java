1d4718fbf2d1046a327f23f1dd5a052f62576975
Mabille <gabi.mabs@gmail.com>
2013-11-14 13:05:33 -0300

    Made some more tests

--- DistribuedProject/src/Test/TestProcessus.java
+++ DistribuedProject/src/Test/TestProcessus.java
@@ -1,25 +1,37 @@
 package Test;
 
+import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.ArrayList;
 
+import CriticalResources.CRWaitingList;
+import CriticalResources.CRWaitingListCell;
+import Participant.Participant;
+import Participant.ParticipantList;
+import Sources.LogicalClock;
 import Sources.Processus;
+import Sources.ServiceThread;
 
 public class TestProcessus {
 	public static void runTest() {
 		System.out.println("####################");
 		System.out.println("## TEST PROCESSUS ##");
 		System.out.println("####################");
-//		testProcessus_1();
-//		System.out.println("############################");
-//		testProcessus_2();
-//		System.out.println("############################");
-//		testProcessus_3();
-//		System.out.println("############################");
-//		testProcessus_4();
-//		System.out.println("############################");
-//		testProcessus_5();
-//		System.out.println("############################");
-		testProcessus_6();
+		//		testProcessus_1();
+		//		System.out.println("############################");
+		//		testProcessus_2();
+		//		System.out.println("############################");
+		//		testProcessus_3();
+		//		System.out.println("############################");
+		//		testProcessus_4();
+		//		System.out.println("############################");
+		//		testProcessus_5();
+		//		System.out.println("############################");
+		//		testProcessus_6();
+		//		System.out.println("############################");
+		//		testProcessus_7();
+		//		System.out.println("############################");
+		testProcessus_8();
 		System.out.println("############################");
 	}
 
@@ -128,8 +140,8 @@ public class TestProcessus {
 			e.printStackTrace();
 		}		
 	}
-	
-	
+
+
 	public static void testProcessus_5() {
 		System.out.println("Test n°5");
 		System.out.println("Description : Test whoHasResource");
@@ -138,7 +150,7 @@ public class TestProcessus {
 			Processus proc = new Processus(1900);
 			int pid = proc.whoHasResource("a1");
 			String result = String.valueOf(pid);
-			
+
 			String attendu = "1900";
 			String obtenu = result;
 
@@ -156,7 +168,7 @@ public class TestProcessus {
 			e.printStackTrace();
 		}		
 	}
-	
+
 	public static void testProcessus_6() {
 		System.out.println("Test n°6");
 		System.out.println("Description : Test whoHasResource with a ressource not inside the resource list");
@@ -165,9 +177,105 @@ public class TestProcessus {
 			Processus proc = new Processus(1900);
 			int pid = proc.whoHasResource("c1");
 			String result = String.valueOf(pid);
-			
+
 			String attendu = "-2";
 			String obtenu = result;
+
+			System.out.println("Attendu : " + attendu);
+			System.out.println("Obtenu  : " + obtenu);
+
+			if (attendu.equals(obtenu)) {
+				System.out.println("Test OK");
+			}
+			else {
+				System.out.println("Test failed");
+			}		
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+
+	public static void testProcessus_7() {
+		System.out.println("Test n°7");
+		System.out.println("Description : Test whoIsHeadOfCRWaitingList with no one in the CRWaitingList");
+		System.out.println("Description : ");
+		try {
+			DatagramSocket s = new DatagramSocket(Processus.portDefault);
+			CRWaitingList[] crList = new CRWaitingList[3];
+			crList[0] = new CRWaitingList();
+			crList[1] = new CRWaitingList();
+			crList[2] = new CRWaitingList();
+			String[] resources = {"a1","b2"};
+			ArrayList<String> resourcesList = new ArrayList<String>();
+			for (int i=0; i<resources.length; i++) {
+				resourcesList.add(resources[i]);
+			}
+			ParticipantList participants = new ParticipantList();
+			int pid = 1900;
+			LogicalClock clock = new LogicalClock();
+			Participant first = new Participant(pid, Processus.portDefault, resourcesList);
+			participants.add(first);
+			ServiceThread serviceT = new ServiceThread(s, crList, resources , participants, pid, clock);
+			serviceT.start();
+
+			Processus proc = new Processus(1920);
+			int pid_r = proc.whoIsHeadOfCRWaitingList(1);
+			String result = String.valueOf(pid_r);
+
+			serviceT.stop();
+			String attendu = "-2";
+			String obtenu = result;
+
+			System.out.println("Attendu : " + attendu);
+			System.out.println("Obtenu  : " + obtenu);
+
+			if (attendu.equals(obtenu)) {
+				System.out.println("Test OK");
+			}
+			else {
+				System.out.println("Test failed");
+			}		
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+
+	public static void testProcessus_8() {
+		System.out.println("Test n°8");
+		System.out.println("Description : Test whoIsHeadOfCRWaitingList with someone in the CRWaitingList");
+		System.out.println("Description : ATTENTION COPYING WHAT IS INSIDE THE FUNCTION FOR THE TEST. NOT REALLY CALLING THE FUNCTION");
+		try {
+			DatagramSocket s = new DatagramSocket(Processus.portDefault);
+			CRWaitingList[] crList = new CRWaitingList[3];
+			crList[0] = new CRWaitingList();
+			crList[1] = new CRWaitingList();
+			crList[2] = new CRWaitingList();
+			String[] resources = {"a1","b2"};
+			ArrayList<String> resourcesList = new ArrayList<String>();
+			for (int i=0; i<resources.length; i++) {
+				resourcesList.add(resources[i]);
+			}
+			ParticipantList participants = new ParticipantList();
+			int pid = 1900;
+			LogicalClock clock = new LogicalClock();
+			Participant first = new Participant(pid, Processus.portDefault, resourcesList);
+			participants.add(first);
+			ServiceThread serviceT = new ServiceThread(s, crList, resources , participants, pid, clock);
+			serviceT.start();
+
+			Processus proc = new Processus(1920);
+			CRWaitingListCell cell = new CRWaitingListCell(1920,proc.getClock().getClock());
+			proc.sendAndRetrieveOneMessage("GET_CRITICAL_REGION<<1<<"+cell+"<<"+ proc.getClock().getClock(), Processus.portDefault);
+			
+			
+			int pid_r = crList[1].get(0).getPid();
+			String result = String.valueOf(pid_r);
+
+			String attendu = "1920";
+			String obtenu = result;
+			serviceT.stop();
 
 			System.out.println("Attendu : " + attendu);
 			System.out.println("Obtenu  : " + obtenu);
